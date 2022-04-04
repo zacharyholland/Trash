e0d7ed429a456c2ae2452d04b7c3785105f8e655
zacharyholland <90477622+zacharyholland@users.noreply.github.com>
2022-04-04 08:36:45 -0600

    Add files via upload

---
+++ AccidentDatabase/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AccidentDatabase.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
